1f3172c6520843b02d8fc12c8fa1ae03a087eaea
root <root@LAPTOP-5881SAH0>
2026-01-12 08:13:59 +0300

    Добавлено создание расширенных признаков для пользователей и книг, создана матрица схожести книг с предпочтениями пользователей

--- app/models.py
+++ app/models.py
@@ -9,10 +9,89 @@ from surprise import accuracy
 from surprise.model_selection import GridSearchCV
 
 #########################################################
-# Подготовка
+# Подготовка данных
 #########################################################
+def add_user_features(df_ratings):
+    """Функция рассчитывает расширенные признаки пользователей."""
+    # Средняя оценка пользователя
+    user_avg_rating = df_ratings.groupby('user_id')['rating'].mean().rename('avg_user_rating')
+    # Количество оценок пользователя
+    user_num_ratings = df_ratings.groupby('user_id')['rating'].count().rename('num_user_ratings')
+    # Активность пользователя
+    total_unique_books = len(df_ratings['book_id'].unique())
+    user_activity = user_num_ratings / total_unique_books * 100
+    user_activity.name = 'user_activity'
+    
+    df_users = pd.concat([user_avg_rating, user_num_ratings, user_activity], axis=1)
+    df_users = df_users.reset_index()
+    return df_users
+
+def add_book_features(df_ratings, df_books, df_book_tags):
+    """Функция рассчитывает расширенные признаки книг."""
+    # Популярность книги
+    book_popularity = df_ratings.groupby('book_id')['user_id'].nunique().rename('popularity').to_frame()
+    # Разнообразие оценок (стандартное отклонение)
+    book_rating_std = df_ratings.groupby('book_id')['rating'].std().rename('rating_std').to_frame()
+    # Тематическая категория
+    # Отбираем самый популярный тэг
+    top_tag_per_book = df_book_tags.groupby('goodreads_book_id')['count'].idxmax()
+    df_top_tags = df_book_tags.iloc[top_tag_per_book].copy()
+    df_top_tags.rename(columns={'tag_id': 'top_tag', 'goodreads_book_id': 'book_id'}, inplace=True)
+    df_top_tags.drop(columns=['count'], inplace=True)
+    
+    # Добавляем признаки
+    df_books_with_tags = df_books.merge(df_top_tags, on='book_id', how='left')
+    df_books_extended = df_books_with_tags.merge(book_popularity, on='book_id', how='left')
+    df_books_extended = df_books_extended.merge(book_rating_std, on='book_id', how='left')
+
+    # Оставляем только существенные признаки
+    df_books_extended = df_books_extended[['id', 'book_id', 'best_book_id', 'authors', 
+                                           'original_title', 'title', 'language_code', 'average_rating', 
+                                           'ratings_count', 'work_ratings_count', 'work_text_reviews_count', 
+                                           'top_tag', 'popularity', 'rating_std']]
+    
+    return df_books_extended
+
+def generate_user_book_similarity_matrix(df_ratings, df_books, df_book_tags, df_tags):
+    """Функция создает матрицу схожестей книг с пользовательскими предпочтениями"""
+    # Создаем текстовый профиль и tf_idf-матрицу на его основе
+    books_profile, tfidf_matrix = create_similarity_matrix(df_book_tags, df_tags, df_books)
+    
+    # Создаем матрицу схожестей пользователей и книг 
+    users = df_ratings["user_id"].unique()
+    books = books_profile["book_id"].unique()
+    interaction_matrix = np.zeros((len(users), len(books)))
+    
+    # Оцениваем схожесть с историей пользователя
+    for user_id in users:
+        # Оценённые книги пользователя
+        rated_books = df_ratings[df_ratings["user_id"] == user_id]["book_id"]
+        
+        # Пропускаем, если нет оценок
+        if len(rated_books) == 0:
+            continue
+        
+        # Пропускаем, если у оцененных нет тегов
+        rated_indices = books_profile[books_profile["book_id"].isin(rated_books)].index
+        if len(rated_indices) == 0:
+            continue
+        
+        # Находим и нормируем косинусное сходство оцененных и неоцененных книг
+        rated_tfidf = tfidf_matrix[rated_indices]
+        similarity = cosine_similarity(np.asarray(rated_tfidf.mean(axis=0)).reshape(1,-1), tfidf_matrix).flatten()
+        scaled_similarity = similarity * 100
+        
+        # Записываем в матрицу
+        interaction_matrix[np.where(users == user_id)[0][0], :] = scaled_similarity
+    
+    df_interaction = pd.DataFrame(interaction_matrix, index=users, columns=books)
+    
+    return df_interaction
+
 def make_datasets(data_path):
-    """Функция для создания и предобработки датасетов"""
+    """Функция для создания и предобработки датасетов."""
+    global df_interaction_matrix
+    
     # Загрузка данных
     df_ratings = pd.read_csv(data_path + '/ratings.csv')
     df_books = pd.read_csv(data_path + '/books.csv')
@@ -22,7 +101,39 @@ def make_datasets(data_path):
     # Предобработка с учетом предыдущей работы
     df_ratings = df_ratings.groupby(['user_id', 'book_id'])['rating'].mean().reset_index()
 
-    return df_ratings, df_books, df_tags, df_book_tags
+    # Создание расширенных признаков
+    df_users = add_user_features(df_ratings)
+    df_books = add_book_features(df_ratings, df_books, df_book_tags)
+    
+    # Рассчитываем матрицу взаимодействий, если еще не рассчитана
+    try:
+        df_interaction_matrix
+    except NameError:
+        df_interaction_matrix = generate_user_book_similarity_matrix(df_ratings, df_books, df_book_tags, df_tags)
+    
+
+    return df_ratings, df_books, df_tags, df_book_tags, df_users, df_interaction_matrix
+
+#########################################################
+# Подготовка моделей
+#########################################################
+def create_similarity_matrix(df_book_tags, df_tags, df_books, N=5):
+    """Функция создает матрицу tf-idf профилей книг."""
+    # Создаем текстовый профиль и tf_idf-матрицу на его основе
+    # Найдем названия тегов по справочнику
+    cont_book_tags = pd.merge(df_book_tags, df_tags, on='tag_id', how='left')
+    cont_book_tags = cont_book_tags.groupby('goodreads_book_id')['tag_name'].apply(lambda x: sorted(x)).reset_index()
+    # Соединим теги и книги
+    cont_books = pd.merge(df_books, cont_book_tags, left_on='book_id', right_on='goodreads_book_id', how='left')
+    # Создадим текстовый профиль для каждой книги
+    cont_books['profile'] = cont_books['original_title'] + ' ' + cont_books['tag_name'].astype(str)
+    books_profile = cont_books[['book_id', 'profile']]
+    
+    # Векторизуем текстовые проифли с помощью TF-IDF
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(books_profile['profile'].fillna('unknown'))
+
+    return books_profile, tfidf_matrix
 
 def prepare_model_svd(df_ratings):
     """Функция подбирает гиперпараметры для модели матричной факторизации (SVD) и производит обучение модели."""
@@ -79,48 +190,36 @@ def get_popularity_recommendation_ids(df_ratings, N=5):
     # Возвращаем id топ-N книг
     return popular_book_ids
 
-def get_similar_books_ids(book_id, 
-                      df_book_tags, 
-                      df_tags, 
-                      df_books, 
-                      N=5):
+def get_similar_books_ids(book_id, df_book_tags, df_tags, df_books, N=5):
     """Контентная модель (похожие книги по тегам и названиям).
 Функция для поиска похожих книг по косинусной мере близости между TF-IDF векторами."""
     # Проверка
     if book_id not in set(df_book_tags['goodreads_book_id']) \
      or book_id not in set(df_books['book_id']):
-        print('''Нет тегов или информации для книги с таким ID={book_id}''')
-        popular_book_ids = get_popularity_recommendation_ids(df_ratings, N=5)
-        return popular_book_ids
-    
-    # Создаем текстовый профиль и tf_idf-матрицу на его основе
-    # Найдем названия тегов по справочнику
-    cont_book_tags = pd.merge(df_book_tags, df_tags, on='tag_id', how='left')
-    cont_book_tags = cont_book_tags.groupby('goodreads_book_id')['tag_name'].apply(lambda x: sorted(x)).reset_index()
-    # Соединим теги и книги
-    cont_books = pd.merge(df_books, cont_book_tags, left_on='book_id', right_on='goodreads_book_id', how='left')
-    # Создадим текстовый профиль для каждой книги
-    cont_books['profile'] = cont_books['original_title'] + ' ' + cont_books['tag_name'].astype(str)
-    
-    # Векторизуем текстовые проифли с помощью TF-IDF
-    vectorizer = TfidfVectorizer()
-    tfidf_matrix = vectorizer.fit_transform(cont_books['profile'].fillna('unknown'))
+        return f'''Нет тегов или информации для книги с таким ID={book_id}: 
+book_tags: {df_book_tags[df_book_tags['goodreads_book_id'] == book_id].values.tolist()}
+books:     {df_books[df_books['book_id'] == book_id].values.tolist()}'''
 
-    # Находим индекс книги        
-    idx = cont_books[cont_books['book_id'] == book_id].index[0]
+    global books_profile, tfidf_matrix
+    try:
+        books_profile, tfidf_matrix
+    except NameError:
+        books_profile, tfidf_matrix = create_similarity_matrix(df_book_tags, df_tags, df_books, N=5)
+    finally:
+        # Находим индекс книги        
+        idx = books_profile[books_profile['book_id'] == book_id].index[0]
+        
+        # Вычисляем косинусную близость
+        similarity_scores = cosine_similarity(tfidf_matrix[idx], tfidf_matrix).flatten()
+        
+        # Получаем индексы N самых похожих книг
+        similar_indices = similarity_scores.argsort()[-N-1:-1][::-1]
     
-    # Вычисляем косинусную близость
-    similarity_scores = cosine_similarity(tfidf_matrix[idx], tfidf_matrix).flatten()
-    
-    # Получаем индексы N самых похожих книг
-    similar_indices = similarity_scores.argsort()[-N-1:-1][::-1]
+        similar_book_ids = books_profile.iloc[similar_indices]['book_id'].values.tolist()
+        
+        # Возвращаем id N самых похожих книг
+        return similar_book_ids
 
-    similar_book_ids = cont_books.iloc[similar_indices]['book_id'].values.tolist()
-    
-    # Возвращаем id N самых похожих книг
-    return similar_book_ids
-
-# Создадим функцию для предсказания оценки по заданной книге и пользователю
 def get_predict_rating_value(user_id, book_id, df_ratings, K=5):
     """Коллаборативная фильтрация (Item-Based).
 Функция предсказывает оценку пользователя для заданной книги."""
@@ -172,7 +271,6 @@ def get_predict_rating_value(user_id, book_id, df_ratings, K=5):
     
     return predicted_rating
 
-# Создадим функцию, которая для заданного пользователя возвращает топ-N книг с наибольшим предсказанным рейтингом
 def get_recommendations_svd(user_id, df_ratings, N=5):
     """Матричная факторизация (SVD).
 Функция возвращает топ-N книг с наибольшим предсказанным рейтингом для заданного пользователя"""
@@ -241,12 +339,12 @@ def main_models(data_path, book_id, user_id):
 # Определяем запуск из-под скрипта:
 if __name__ == '__main__':
     data_path = os.path.abspath('data')
-    book_id = int(input('ID книги:'))
-    user_id = int(input('ID пользователя:'))
+    book_id = 6621
+    user_id = 315
 
     # Загружаем даныне
     print('Загружаем данные...')
-    df_ratings, df_books, df_tags, df_book_tags = make_datasets(data_path)
+    df_ratings, df_books, df_tags, df_book_tags, df_users, df_interaction_matrix = make_datasets(data_path)
 
     # Обучаем модель SVD
     print('Обучаем модель SVD...')
@@ -260,5 +358,5 @@ if __name__ == '__main__':
     print(similar_book_ids)
     print('Вероятная оценка пользователя для книги:')
     print(predict_rating)
-    print('Наиболее подходящие книги для пользователя:')
+    print('Наиболее подходящие книги для пользователя (SVD):')
     print(top_books_for_user)
